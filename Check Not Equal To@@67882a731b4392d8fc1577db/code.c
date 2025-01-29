d8c95c220a39dd31aad6f6df617951c58dbab86a
Jatin Tehri <jatintehri1974@gmail.com>
2025-01-29 18:07:19 +0530

    Solution to Check Not Equal To

---
+++ Check Not Equal To@@67882a731b4392d8fc1577db/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num[2];
+    scanf("%d  %d",&num[0],&num[1]);
+    if(num[0] != num[1]){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+    return 0;
+}
