bdaf3de8fb9c81ad94074adb2a3145564ebd3fb8
Jatin Tehri <jatintehri1974@gmail.com>
2025-02-04 16:46:43 +0530

    Solution to Search Student by Roll Number

--- Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
+++ Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
@@ -10,11 +10,15 @@ int main() {
     int N;
     scanf("%d",&N);
     int req_student;
-    struct Student S[N];
-    for(int i=0;i<N;i++){
+    struct Student S[N+1];
+    for(int i=1;i<=N;i++){
         scanf("%d %s %f",&S[i].roll_number,&S[i].name,&S[i].marks);
     }
     scanf("%d",&req_student);
-    printf("Roll Number: %d, Name: %s, Marks: %.2f",S[req_student].roll_number,S[req_student].name,S[req_student].marks);
+    if(req_student <= N){
+        printf("Roll Number: %d, Name: %s, Marks: %.2f",S[req_student].roll_number,S[req_student].name,S[req_student].marks);
+    }else{
+        printf("Student not  found");
+    }
     return 0;
 }
