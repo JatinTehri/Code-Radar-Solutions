50bcb84857e38ff8f02b6f4142ca41d7d8989eff
Jatin Tehri <jatintehri1974@gmail.com>
2025-02-10 01:46:23 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -9,18 +9,24 @@ struct Book{
 int main() {
     int n;
     scanf("%d",&n);
+    int threshold;
+    int above_threshold = 1;
     struct Book B[n];
     for(int i=0;i<n;i++){
         scanf("%s %s %d",&B[i].title,&B[i].author,B[i].price);
     }
-    int threshold;
+    
     scanf("%d",&threshold);
     
     for(int i=0;i<n;i++){
-        if(B[i].price > threshold){
-            printf("Books above price %.2f:\n",(float) (threshold));
-            printf("Title: %s, Author: %s, Price: %.2f",B[i].title,B[i].author,(float)(B[i].price));
+        if(B[i].price <= threshold){
+            above_threshold = 0;
         }
+        break;
+    }
+    if(above_threshold){
+          printf("Books above price %.2f:\n",(float) (threshold));
+            printf("Title: %s, Author: %s, Price: %.2f",B[i].title,B[i].author,(float)(B[i].price));
     }
 
     return 0;
