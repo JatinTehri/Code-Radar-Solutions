a1ebb652a0b71fbb3d145b1bf52c0c9139e80273
Jatin Tehri <jatintehri1974@gmail.com>
2025-02-11 23:39:25 +0530

    Solution to Movie Ticket Booking System

--- Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
+++ Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
@@ -1,32 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-struct MovieTicket{
+struct MovieTicket {
     char movie_name[10];
     char ticket_type[10];
     int ticket_price;
 };
 
-int main(){
+int main() {
     int N;
-    scanf("%d",&N);
+    scanf("%d", &N);
     struct MovieTicket M[N];
     int standard_price = 0;
     int premium_price = 0;
     int vip_price = 0;
-    for(int i = 0;i<N;i++){
-        scanf("%s %s %d",&M[i].movie_name,&M[i].ticket_type,&M[i].ticket_price);
+    for (int i = 0; i < N; i++) {
+        scanf("%s %s %d", M[i].movie_name, M[i].ticket_type, &M[i].ticket_price);
     }
-    for(int i = 0;i<N;i++){
-         if(M[i].ticket_type == "Standard"){
+    for (int i = 0; i < N; i++) {
+         if (strcmp(M[i].ticket_type, "Standard") == 0) {
             standard_price += M[i].ticket_price;
-         }
-         else if(M[i].ticket_type == "Premium"){
+         } else if (strcmp(M[i].ticket_type, "Premium") == 0) {
             premium_price += M[i].ticket_price;
-         }
-         else{
+         } else {
             vip_price += M[i].ticket_price;
          }
     }
-    printf("Standard: %.2f, Premium: %.2f, VIP: %.2f",(float)(standard_price),(float)(premium_price),(float)(vip_price));
+    printf("Standard: %.2f, Premium: %.2f, VIP: %.2f", (float)(standard_price), (float)(premium_price), (float)(vip_price));
     return 0;
 }
