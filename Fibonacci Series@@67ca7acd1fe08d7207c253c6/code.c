139059d77b01e15ca07ccbb73d3a1025c0d89465
Jatin Tehri <jatintehri1974@gmail.com>
2025-03-09 15:59:14 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,12 @@
+void fibo (int n){
+    int arr[n];
+    arr[0] = 0;
+    arr[1] = 1;
+    for(int i=2;i<n;i++){
+        arr[i] = fibo(n-1) + fibo(n-2);
+    }
+    for(int j=0;j<n;j++){
+        printf("%d ",arr[j]);
+    }
+    
+}
